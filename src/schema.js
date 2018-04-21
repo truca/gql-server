eae41aa27d82782c1355e9bddd1de08d694e447e
Ignacio Ureta <ignacio.ureta@live.com>
2018-04-21 15:40:08 -0300

    using forms model

--- src/schema.js
+++ src/schema.js
@@ -1,13 +1,32 @@
-import { makeExecutableSchema } from 'graphql-tools';
-import typeDefs from './types.graphql';
-import users from '../data/users';
+import { makeExecutableSchema } from "graphql-tools";
+import typeDefs from "./types.graphql";
+import users from "../data/users";
+import axios from "axios";
+
+const baseURL = "http://1b773838.ngrok.io/api";
+const resMapper = res => res.data;
 
 const resolvers = {
-    Query: {
-        user: (_, args) => users.find(user => user.id === args.id),
-        totalUsers: () => users.length,
-        allUsers: () => users,
+  Query: {
+    form: (_, args) => {
+      return axios
+        .get(`${baseURL}/form/${args.id}`)
+        .then(resMapper)
+        .then(form => form);
+    },
+    totalForms: () => {
+      return axios
+        .get(`${baseURL}/forms`)
+        .then(resMapper)
+        .then(forms => forms.length);
+    },
+    allForms: () => {
+      return axios
+        .get(`${baseURL}/forms`)
+        .then(resMapper)
+        .then(forms => forms);
     }
-}
+  }
+};
 
 export default makeExecutableSchema({ typeDefs, resolvers });
